1552e7ecb4d27cf449fb9adc6e708b3e8d6796c1
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-15 19:22:21 -0400

    fix

--- spec/controllers/cities_controller_spec.rb
+++ spec/controllers/cities_controller_spec.rb
@@ -33,17 +33,16 @@ RSpec.describe CitiesController, :type => :controller do
     end
 
     it 'returns one city with its neighborhoods as a json attribute' do 
-
+      get :show, format: :json, id: 1
+      body = JSON.parse(response.body)
+      first_nabe = body["neighborhoods"][0]["id"]
+      expect(response.status).to eq 200
+      expect(first_nabe).to eq(@nabe1.id)
     end
   end
 
   describe 'GET /cities/most_res' do 
     xit 'returns city with the most reservations' do 
-      get :show, format: :json, id: 1
-      body = JSON.parse(response.body)
-      nabes = body["neighborhoods"]
-      expect(response.status).to eq 200
-      expect(nabes.count).to eq(3)
     end
   end
 
