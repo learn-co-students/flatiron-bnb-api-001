68ed72797e88de632221147457e3db0482588832
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-15 18:51:38 -0400

    user tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -32,28 +32,68 @@ RSpec.describe UsersController, :type => :controller do
       expect(user_host).to eq(User.first.host)
     end
 
-    xit 'returns their listings as a json attribute' do 
+    it 'returns their listings as a json attribute' do 
+      get :show, format: :json, id: 1
+      body = JSON.parse(response.body)
+      first_listing = body["listings"][0]["address"]
+      expect(response.status).to eq 200
+      expect(first_listing).to eq(@listing1.address)
     end
 
-    xit 'returns their reservations as a json attribute' do
+    it 'returns their reservations as a json attribute' do
+      get :show, format: :json, id: 1
+      body = JSON.parse(response.body)
+      first_reservation = body["reservations"][0]["id"]
+      expect(response.status).to eq 200
+      expect(first_reservation).to eq(@reservation1.id)
     end 
 
-    xit 'returns their reservation count as a json attribute' do
+    it 'returns their reservation count as a json attribute' do
+      get :show, format: :json, id: 1
+      body = JSON.parse(response.body)
+      res_count = body["res_count"]
+      expect(response.status).to eq 200
+      expect(res_count).to eq(User.first.reservations.count)
     end 
 
-    xit 'returns their trips as a json attribute' do 
+    it 'returns their trips as a json attribute' do 
+      get :show, format: :json, id: 6
+      body = JSON.parse(response.body)
+      first_trip = body["trips"][0]["id"]
+      expect(response.status).to eq 200
+      expect(first_trip).to eq(@reservation3.id)
     end
 
-    xit 'if present, returns their hosts as a json attribute' do 
+    it 'if present, returns their hosts as a json attribute' do 
+      get :show, format: :json, id: 6
+      body = JSON.parse(response.body)
+      first_host = body["hosts"][0]["name"]
+      expect(response.status).to eq 200
+      expect(first_host).to eq(@arel.name)
     end
 
-    xit 'if present, returns their guests as a json attribute' do 
+    it 'if present, returns their guests as a json attribute' do 
+      get :show, format: :json, id: 3
+      body = JSON.parse(response.body)
+      first_guest = body["guests"][0]["name"]
+      expect(response.status).to eq 200
+      expect(first_guest).to eq(@avi.name)
     end
 
-    xit 'if host, returns their reviews from guests' do 
+    it 'if host, returns their reviews from guests' do 
+      get :show, format: :json, id: 3
+      body = JSON.parse(response.body)
+      first_review = body["host_reviews"][0]["id"]
+      expect(response.status).to eq 200
+      expect(first_review).to eq(@review3.id)
     end
 
-    xit 'if host, returns their host_reviews count' do
+    it 'if host, returns their host_reviews count' do
+      get :show, format: :json, id: 3
+      body = JSON.parse(response.body)
+      review_count = body["host_review_count"]
+      expect(response.status).to eq 200
+      expect(review_count).to eq(1)
     end
   end
 
